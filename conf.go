1928dbe28bf7bd108327ef86714fece1b0bd7b1d
lhdht <15229684090@163.com>
2026-01-09 14:23:07 +0800

    fix: remove DialTimeout

--- conf.go
+++ conf.go
@@ -1,11 +1,9 @@
 package etcd
 
-import "time"
-
 type Conf struct {
-	Username    string        `json:"account"`
-	Password    string        `json:"password"`
-	Tls         *TLS          `json:"tls"`
-	Endpoint    []string      `json:"endpoint"`
-	DialTimeout time.Duration `json:"dial_timeout"`
+	Username string   `json:"account"`
+	Password string   `json:"password"`
+	Endpoint []string `json:"endpoint"`
+
+	Tls *TLS `json:"tls"`
 }
