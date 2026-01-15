4458b6c6a9bb107377d79c05de77ca385e76a370
lhdht <15229684090@163.com>
2026-01-15 09:54:09 +0800

    fix: tls connect error bug, set server name

--- core.go
+++ core.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"time"
 
+	"github.com/fireflycore/go-utils/network"
 	"github.com/fireflycore/go-utils/tlsx"
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
@@ -32,6 +33,14 @@ func New(c *Conf) (*clientv3.Client, error) {
 	// 启用 TLS 时，将 TLSConfig 写入 clientOptions。
 	if tlsEnabled {
 		config.TLS = tlsConfig
+		// 若未设置 ServerName，则尝试从 Endpoints 中解析第一个主机名作为 ServerName
+		if len(c.Endpoint) > 0 {
+			host, _, err := network.SplitHostPort(c.Endpoint[0], "2379")
+			if err != nil {
+				return nil, err
+			}
+			config.TLS.ServerName = host
+		}
 	}
 
 	return clientv3.New(config)
